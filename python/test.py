951c235a6409560c6142385a539687d541c59bdc
Ale Santoro <alexi.santoro@gmail.com>
2024-01-04 20:14:57 +0100

    added script to cap 3, did most of combinations in cap 4

--- python/test.py
+++ python/test.py
@@ -5,29 +5,34 @@ from pattFinder.pattfinder import *
 # allows to find out if the specified patterns cover all the permutations of lenght n
 # that are not sortable by the specified sorting operator
 
-if len(sys.argv) < 4:
-	print("ERROR; requested at least 3 arguments but got: " + str(sys.argv[1:]))
-	exit()
+if __name__ == '__main__':
 
-op = getOperator(sys.argv[1])
-n = int(sys.argv[2])
-av = sys.argv[3:]
+	if len(sys.argv) < 4:
+		print("ERROR; requested at least 3 arguments but got: " + str(sys.argv[1:]))
+		exit()
 
-print("Generating unsortable permutations")
-unsortable = selectorPermutations(n,op).getUnsortable()
+	op = getOperator(sys.argv[1])
+	n = int(sys.argv[2])
+	av = sys.argv[3:]
 
-print("Verifying "+str(len(av))+" patterns")
-for p in av:
-	print("Verifying pattern " + p)
-	# from the list of unsortable premutations the ones containing the specified patterns get removed
-	pattern  = [int(char) for char in p]
-	contains_p = PatternAvoid(n, pattern).getContaining()
-	unsortable = list(set(unsortable).difference(set(contains_p)))
+	print("Generating permutations: ")
+	unsortable = selectorPermutations(n,op).getUnsortable()
+	result = unsortable
+	print("\tfound " + str(len(unsortable)) + " unsortable permutations\n")
+	
+	print("Verifying "+str(len(av))+" patterns:\n")
+	for p in av:
+		print("Verifying pattern " + p+":")
+		# from the list of unsortable premutations the ones containing the specified patterns get removed
+		pattern  = [int(char) for char in p]
+		contains_p = PatternAvoid(n, pattern).getContaining()
+		result  = list(set(result).difference(set(contains_p)))
+		print("\tfound "+str(len(set(contains_p).intersection(set(unsortable)))) + " matches\n")
 
-if(len(unsortable)==0):
-	print("\nAll unsortable permutations contains some of the specified patterns\n")
-else:
-	print("\nThese permutations are not sorted by " + sys.argv[1]+" and not contain any specified pattern:")
-	for x in unsortable:
-		print(str(x))
-	print("\n")
+	n_uns = len(result)
+	if(n_uns==0):
+		print("All unsortable permutations contains some of the specified patterns\n")
+	else:
+		print("These "+str(n_uns)+" permutations are not sorted by " + sys.argv[1]+" but not contain any specified pattern:")
+		print(printlist(result))
+
